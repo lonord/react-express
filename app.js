9e5822ac4bf33453ec7b63034fc5270cd26f7615
lonord <lonord@qq.com>
2017-03-16 15:12:12 +0800

    some improvement

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -32,23 +32,25 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        let status = err.status || 500;
+        res.status(status);
+        res.json({
+            msg: err.message
+        });
+        if (status == 500) {
+            console.error(err.stack);
+        }
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        msg: err.message
+    });
 });
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,27 @@
 var webpack = require('webpack');
 
+var isDev = process.env.NODE_ENV != 'production';
+
+var plugins = [];
+
+if (!isDev) {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+        beautify: true,
+        mangle: {
+            screw_ie8: true,
+            keep_fnames: true
+        },
+        compress: {
+            screw_ie8: true
+        },
+        comments: false
+    }));
+}
+
 module.exports = {
+    debug: isDev,
+    devtool: isDev ? 'source-map' : false,
+    watch: isDev,
 	entry: [
 		'./components/entry.js'
 	],	
@@ -19,5 +40,6 @@ module.exports = {
     		test: /\.(png|jpg)$/,
     		loader: 'url-loader?limit=8192'
     	}]
-	}
+	},
+    plugins: plugins
 }
